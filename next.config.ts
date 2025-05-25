9a663a4aefdd941878b2be08fc758ebd9d62d360
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-05-25 15:52:48 +0900

    feat: `WebP`/`AVIF` 이미지 포맷 설정 및 `@next/bundle-analyzer` 적용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 };
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
 
 export default nextConfig;
