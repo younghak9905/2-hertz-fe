cdafdbdeb1d98f98b3caea2231b66512070258a7
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-05-30 23:49:38 +0900

    `prefixEnum` 도입으로 키워드 ENUM 중복 및 이모지 렌더링 오류 해결 (#67)
    * refactor: enum 키워드 중복 방지를 위한 prefixEnum 적용
    
    - prefixEnum 유틸 도입으로 enum 키 중복 문제 해결
    - `ALL_KEYWORD_MAP` 구조 정비
    - 공통 관심사 키워드 중 선호하는 상대방 성향에만 이모지 적용 ( 👩🏻‍❤️‍👨🏻)
    
    * refactor: 키워드 데이터에 prefix 적용 및 렌더링 일관성 확보
    
    - 사용자 키워드와 공통 키워드 모두 '카테고리_KEY' 포맷으로 변환
    - `normalizeCategory` 유틸 함수 추가로 camelCase → SNAKE_CASE 변환
    - KeywordTag 컴포넌트와 포맷 일관성을 유지하도록 리팩토링
    
    * style: 불필요한 키워드 컴포넌트 제거 및 온보딩 UI 수정

--- src/app/onboarding/interests/page.tsx
+++ src/app/onboarding/interests/page.tsx
@@ -8,14 +8,16 @@ const UserPreferenceForm = dynamic(
   () => import('@/components/onboarding/interests/UserPreferenceForm'),
   { ssr: false },
 );
+
 export default function OnboardingInterestsPage() {
   const [step, setStep] = useState(1);
+
   return (
-    <>
+    <div className="mx-auto flex h-screen max-w-[430px] flex-col">
       <Header title="취향 선택하기" showBackButton={step > 1} showNotificationButton={false} />
-      <main className="overflow-y-auto p-4">
+      <div className="flex-1 px-4 pt-4">
         <UserPreferenceForm onStepChange={setStep} />
-      </main>
-    </>
+      </div>
+    </div>
   );
 }
--- src/components/matching/individual/KeywordTagGroup.tsx
+++ src/components/matching/individual/KeywordTagGroup.tsx
@@ -2,7 +2,12 @@
 
 import KeywordTag from '@/components/common/KeywordTag';
 import { TuningKeywords, TuningSameInterests } from '@/lib/api/matching';
+import { useEffect } from 'react';
 
+interface Tag {
+  key: string;
+  label: string;
+}
 interface KeywordTagGroupProps {
   keywords: TuningKeywords;
   sameInterests: TuningSameInterests;
@@ -14,8 +19,20 @@ export default function KeywordTagGroup({
   sameInterests,
   nickname,
 }: KeywordTagGroupProps) {
-  const keywordList = Object.values(keywords);
-  const commonInterestList = Object.values(sameInterests).flat();
+  // 사용자 키워드를 렌더링용 문자열 배열로 변환 ('preferredPeople': 'RELIABLE' → 'PREFERRED_PEOPLE_RELIABLE')
+  const keywordList = Object.entries(keywords).map(
+    ([category, key]) => `${category.toUpperCase()}_${key}`,
+  );
+
+  const commonInterestList = Object.entries(sameInterests).flatMap(([category, keys]) => {
+    const normalizedCategory = normalizeCategory(category);
+    return keys.map((key: Tag) => `${normalizedCategory}_${key}`);
+  });
+
+  //  camelCase → SNAKE_CASE 변환 유틸 함수'preferredPeople' → 'PREFERRED_PEOPLE'
+  function normalizeCategory(category: string) {
+    return category.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase();
+  }
 
   return (
     <main className="space-y-4 px-4">
