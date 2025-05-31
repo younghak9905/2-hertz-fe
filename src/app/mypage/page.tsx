315373a58782f08c89d61a818d2528a8794e4534
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-05-31 18:54:15 +0900

    feat: 로그아웃 / 매칭 동의용 모달 퍼블리싱

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import { useState } from 'react';
 import Header from '@/components/layout/Header';
 import KeywordTagGroup from '@/components/matching/individual/KeywordTagGroup';
 import UserProfileCard from '@/components/mypage/UserProfileCard';
+import { ConfirmModal } from '@/components/common/ConfirmModal';
 
 const matchedUser = {
   code: 'TUNING_SUCCESS',
@@ -32,6 +36,11 @@ const matchedUser = {
 };
 
 export default function MyPage() {
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+  const handleLogout = () => {
+    setIsLogoutModalOpen(false);
+  };
+
   return (
     <>
       <Header title="마이페이지" showBackButton={false} showNotificationButton={true} />
@@ -51,7 +60,30 @@ export default function MyPage() {
             />
           </div>
         </div>
+
+        <div className="mt-10 flex justify-center px-8">
+          <button
+            onClick={() => setIsLogoutModalOpen(true)}
+            className="border-b-1 border-[var(--gray-400)] text-xs font-semibold text-[var(--gray-400)]"
+          >
+            로그아웃
+          </button>
+        </div>
       </main>
+      <ConfirmModal
+        isOpen={isLogoutModalOpen}
+        title="정말 로그아웃 하시겠어요?"
+        description={
+          <>
+            로그아웃 버튼 클릭 시, 계정은 유지되며
+            <br /> 언제든 다시 로그인 할 수 있어요.
+          </>
+        }
+        confirmText="로그아웃하기"
+        onConfirm={handleLogout}
+        onCancel={() => setIsLogoutModalOpen(false)}
+        variant="quit"
+      />
     </>
   );
 }
