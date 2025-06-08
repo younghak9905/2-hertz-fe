b5a8d8c5b723529d5a7a0b3687f392f3651889dd
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-06-08 15:45:08 +0900

    feat: 마이페이지 api 연결

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,47 +1,44 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from '@/components/layout/Header';
 import KeywordTagGroup from '@/components/matching/individual/KeywordTagGroup';
 import UserProfileCard from '@/components/mypage/UserProfileCard';
 import { useConfirmModalStore } from '@/stores/modal/useConfirmModalStore';
-
-const matchedUser = {
-  code: 'TUNING_SUCCESS',
-  message: '튜닝된 사용자가 정상적으로 조회되었습니다.',
-  data: {
-    userId: 13,
-    profileImage: '/images/penguin-profile.png',
-    nickname: '로또에 당첨된 뚱이',
-    gender: 'FEMALE',
-    oneLineIntroduction:
-      '안녕하세요 :) 서로의 이야기에 귀 기울이고, 편안한 분위기 속에서 진심 어린 대화를 나누고 싶어요. 공감과 배려를 소중히 여기며, 깊이 있는 인연으로 이어가고 싶습니다!',
-    keywords: {
-      mbti: 'ISTJ',
-      religion: 'NON_RELIGIOUS',
-      smoking: 'TRYING_TO_QUIT',
-      drinking: 'TRYING_TO_QUIT',
-    },
-    sameInterests: {
-      personality: ['RELIABLE'],
-      preferredPeople: ['RELIABLE'],
-      currentInterests: ['MOVIES'],
-      favoriteFoods: ['TTEOKBOKKI'],
-      likedSports: ['BASEBALL'],
-      pets: ['CAT'],
-      selfDevelopment: ['STUDYING'],
-      hobbies: ['MUSIC'],
-    },
-  },
-};
+import { getUserInfo, GetUserInfoResponse } from '@/lib/api/user';
+import LoadingSpinner from '@/components/common/LoadingSpinner';
 
 export default function MyPage() {
+  const [userInfo, setUserInfo] = useState<GetUserInfoResponse['data'] | null>(null);
   const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   const handleLogout = () => {
     setIsLogoutModalOpen(false);
   };
 
   const openModal = useConfirmModalStore((state) => state.openModal);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const userId = localStorage.getItem('userId');
+
+        if (!userId) {
+          console.error('userId가 없습니다.');
+          return;
+        }
+        const response = await getUserInfo(userId);
+        setUserInfo(response.data);
+      } catch (error) {
+        console.error('유저 정보 불러오기 실패: ', error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
+  if (!userInfo) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <>
@@ -50,15 +47,15 @@ export default function MyPage() {
         <div className="flex w-full flex-grow flex-col justify-between rounded-3xl px-5 py-4">
           <div className="space-y-4">
             <UserProfileCard
-              profileImage={matchedUser.data.profileImage}
-              nickname={matchedUser.data.nickname}
-              oneLineIntroduction={matchedUser.data.oneLineIntroduction}
-              gender={matchedUser.data.gender}
+              profileImage={userInfo.profileImage}
+              nickname={userInfo.nickname}
+              oneLineIntroduction={userInfo.oneLineIntroduction}
+              gender={userInfo.gender}
             />
             <KeywordTagGroup
-              keywords={matchedUser.data.keywords}
-              sameInterests={matchedUser.data.sameInterests}
-              nickname={matchedUser.data.nickname}
+              keywords={userInfo.keywords}
+              sameInterests={userInfo.interests}
+              nickname={userInfo.nickname}
             />
           </div>
         </div>
